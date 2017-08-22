904dfb545536e23857133e9e22fdfc9f53a78609
c.mcguire <cdmcguire000@gmail.com>
2017-08-22 14:54:52 -0700

    create recipe form

---
+++ mysqli_connect.inc.php
@@ -0,0 +1,13 @@
+<?php # mysqli_connect.inc.php
+
+# Create a new connection to the database
+$db = new mysqli('localhost','root','','mealprep');
+
+# If there was an error connecting to the database
+if ($db->connect_error) {
+	$error = $db->connect_error;
+	echo $error;
+} // end if
+
+# Set the character encoding of the database connection to UTF-8
+$db->set_charset('utf8');
