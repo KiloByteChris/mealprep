64b3d669720f2b7dff7c3a6b4657c25244ab6701
c.mcguire <cdmcguire000@gmail.com>
2017-08-24 10:20:52 -0700

    added main page. Main page how form for the days of the week

---
+++ mealprep.php
@@ -0,0 +1,110 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8" >
+	<title>mealprep</title>
+	
+</head>
+
+<body>
+	<header>
+		<h1>Meal Prep</h1>
+		
+		<nav>
+			<a href="newrecipe.html">New Recipe</a>
+			<a href="browse_recipes.php">Browse Recipes</a>
+		</nav>
+	</header>
+	
+	<content>
+		<form action="mealprep_process.php" method="post">
+			<fieldset>
+			<legend>Sunday</legend>
+				<label for="sunBreak">Breakfast</label>
+				<input type="text" name="sunLunch" value="" maxlength="50">
+			
+				<label for="sunLunch">Lunch</label>
+				<input type="text" name="sunLunch" value="" maxlength="50">
+				
+				<label for="sunDinner">Dinner</label>
+				<input type="text" name="sunDinner" value="" maxlength="50">
+				
+			</fieldset>
+			<fieldset>
+			<legend>Monday</legend>
+				<label for="monBreak">Breakfast</label>
+				<input type="text" name="monLunch" value="" maxlength="50">
+			
+				<label for="monLunch">Lunch</label>
+				<input type="text" name="monLunch" value="" maxlength="50">
+				
+				<label for="monDinner">Dinner</label>
+				<input type="text" name="monDinner" value="" maxlength="50">
+				
+			</fieldset>
+			<fieldset>
+			<legend>Tuesday</legend>
+				<label for="tueBreak">Breakfast</label>
+				<input type="text" name="tueLunch" value="" maxlength="50">
+			
+				<label for="tueLunch">Lunch</label>
+				<input type="text" name="tueLunch" value="" maxlength="50">
+				
+				<label for="tueDinner">Dinner</label>
+				<input type="text" name="tueDinner" value="" maxlength="50">
+				
+			</fieldset>
+			<fieldset>
+			<legend>Wednesday</legend>
+				<label for="wedBreak">Breakfast</label>
+				<input type="text" name="wedLunch" value="" maxlength="50">
+			
+				<label for="wedLunch">Lunch</label>
+				<input type="text" name="wedLunch" value="" maxlength="50">
+				
+				<label for="wedDinner">Dinner</label>
+				<input type="text" name="wedDinner" value="" maxlength="50">
+				
+			</fieldset>
+			<fieldset>
+			<legend>Thursday</legend>
+				<label for="thuBreak">Breakfast</label>
+				<input type="text" name="thuLunch" value="" maxlength="50">
+			
+				<label for="thuLunch">Lunch</label>
+				<input type="text" name="thuLunch" value="" maxlength="50">
+				
+				<label for="thuDinner">Dinner</label>
+				<input type="text" name="thuDinner" value="" maxlength="50">
+				
+			</fieldset>
+			<fieldset>
+			<legend>Friday</legend>
+				<label for="friBreak">Breakfast</label>
+				<input type="text" name="friLunch" value="" maxlength="50">
+			
+				<label for="friLunch">Lunch</label>
+				<input type="text" name="friLunch" value="" maxlength="50">
+				
+				<label for="friDinner">Dinner</label>
+				<input type="text" name="friDinner" value="" maxlength="50">
+				
+			</fieldset>
+			<fieldset>
+			<legend>Saturday</legend>
+				<label for="satBreak">Breakfast</label>
+				<input type="text" name="satLunch" value="" maxlength="50">
+			
+				<label for="satLunch">Lunch</label>
+				<input type="text" name="satLunch" value="" maxlength="50">
+				
+				<label for="satDinner">Dinner</label>
+				<input type="text" name="satDinner" value="" maxlength="50">
+				
+			</fieldset>
+		</form>
+	</content>
+
+</body>
+</html>
