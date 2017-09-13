6746da50f042acfe59a0ad89d4031254f17bc8d5
c.mcguire <cdmcguire000@gmail.com>
2017-09-12 22:25:50 -0700

    trying to fix the recipe queries

--- php/search_queries.php
+++ php/search_queries.php
@@ -1,37 +1,25 @@
 <?php
 	
 	require "../database/mysqli_connect.inc.php";
-	function searchQuery() {
-		$type ='';
-		$catagory = '';
-		$search = '';
+	function searchQuery($type, $catagory, $searchBox) {
+		$type = $type;
+		$catagory = $catagory;
+		$search = $search;
+		$returnString = '';
 		
-		//Defining variables from the search form for use in queries
-		if($_SERVER['REQUEST_METHOD'] == 'GET') {
-			if(isset($_GET['typeSelect'])) {
-				$type = $_GET['typeSelect'];
+		if($type != 'tag') {
+			$sql = "SELECT recipe_info.recipe_name FROM recipe, recipe_info WHERE (recipe.recipe_name = recipe_info.recipe_name) AND (recipe_info.$type = '$search') AND (recipe.recipe_catagory = '$catagory') ";
+			$result = $db->query($sql);
+			$returnString += "<ul>";
+			while($rows = $result->fetch_row()){
+				$returnString += "<div class="."draggable"." class="."ui-widget-content".">";
+					$returnString += "<li>";
+						$returnString += "$rows[0]";
+					$returnString += "</li>";
+				$returnString += "</div>";
 			}
-			if(isset($_GET['catagorySelect'])) {
-				$catagory = $_GET['catagorySelect'];
-			}
-			if(isset($_GET['searchBox'])) {
-				$search = $_GET['searchBox'];
-			}
-			
-			
-			if($type != 'tag') {
-				$sql = "SELECT DISTINCT recipe_info.recipe_name FROM recipe, recipe_info WHERE (recipe.recipe_name = recipe_info.recipe_name) AND (recipe_info.$type = '$search') AND (recipe.recipe_catagory = '$catagory') ";
-				$result = $db->query($sql);
-				echo "<ul>";
-				while($rows = $result->fetch_row()){
-					echo "<div class="."draggable"." class="."ui-widget-content".">";
-						echo "<li>";
-							echo "$rows[0]";
-						echo "</li>";
-					echo "</div>";
-				}
-				echo "</ul>";
-			}
+			$returnString += "</ul>";
 		}
+		return $returnString;
 	}
 ?>
