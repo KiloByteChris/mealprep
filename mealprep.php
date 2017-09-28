a771c85bb79c0e34f44567d5b35f8fbb3341f78c
c.mcguire <cdmcguire000@gmail.com>
2017-09-28 15:41:43 -0700

    drag and drop now work. Also added a notes document

--- mealprep.php
+++ mealprep.php
@@ -5,6 +5,11 @@ if(!isset($_SESSION)){
 	session_start();
 }
 
+//Initialize the $searchResults variable
+$searchResults = "";
+//initialize the $calanderString Variable
+$calanderString = "";
+
 # mysqli_connect.inc.php
 
 # Create a new connection to the database
@@ -19,41 +24,41 @@ if ($db->connect_error) {
 # Set the character encoding of the database connection to UTF-8
 $db->set_charset('utf8');
 
-var_dump($_POST);
-$fullList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']; 
+//var_dump($_POST);
+$fullList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 //function creates the form with check boxes for selecting which days of the week
 function createDayCheckboxes($fullList) {
 	$chooseDaysString = "";
 	$chooseDaysString .= "<div id=\"chooseDaysDiv\">";
-		
+
 	for($i = 0; $i < count($fullList); $i++){
 		$day = $fullList[$i];
 		$dayLower = strtolower($day);
 		$dayLabel = $dayLower . "Box";
-		
+
 		$chooseDaysString .= "<label for=" . $dayLabel .">" . $day . "</label>";
-		
+
 		//Sets the default value to not be checked
 		//If the form has been posted and the box is checked, make it sticky
 		$checked = "";
 		if(isset($_POST[$dayLabel])){
 			$checked = "checked";
 		}
-		$chooseDaysString .= "<input type=\"checkbox\" class=\"checkbox\" value=" . $day . " name=" . $dayLabel . " $checked>";	
-			
+		$chooseDaysString .= "<input type=\"checkbox\" class=\"checkbox\" value=" . $day . " name=" . $dayLabel . " $checked>";
+
 	}
-		
+
 	//Select box for choosing the number of servings
 	$chooseDaysString .= "<label for=\"servingsSelect\">Number of Servings</label>";
 	$chooseDaysString .= "<select name=\"servingSelect\" id=\"servingSelect\">";
 	for($i = 1; $i <= 10; $i++){
 		$chooseDaysString .= "<option value=". $i .">" . $i . "</option>";
 	}
-		
+
 	$chooseDaysString .= "</select>";
 	$chooseDaysString .= "<input type=\"submit\" id=\"daysButton\" name=\"createCalander\" value=\"Create\" />";
-		
+
 	$chooseDaysString .= "</div>";
 	return $chooseDaysString;
 }
@@ -62,33 +67,33 @@ $chooseDayString = createDayCheckboxes($fullList);
 
 //Creates the html for each calander day
 function createCalanderDay($day){
-	
+
 	$today = $day;
 	$dayLower = strtolower($today);
 	$breakfastLabel = $dayLower . "Breakfast";
 	$lunchLabel = $dayLower . "Lunch";
 	$dinnerLabel = $dayLower . "Dinner";
-	
+
 	$dayString = "";
 	$dayString .= "<div>";
 	$dayString .= "<fieldset class="."dayField".">";
 	$dayString .= "<legend>" . $day . "</legend>";
-			
+
 	$dayString .= "<label for=" . $breakfastLabel . ">Breakfast</label>";
 	$labelValue1 = checkisset($breakfastLabel);
 	$dayString .= "<input type=\"text\" name=" . $breakfastLabel . " value=\"$labelValue1\" class=\"recipeInput\" maxlength=\"50\">";
-			
+
 	$dayString .= "<label for=" . $lunchLabel . ">Lunch</label>";
 	$labelValue2 = checkisset($lunchLabel);
 	$dayString .= "<input type=\"text\" name=" . $lunchLabel . " value=\"$labelValue2\" class=\"recipeInput\" maxlength=\"50\">";
-	
+
 	$dayString .= "<label for=" . $dinnerLabel . ">Dinner</label>";
 	$labelValue3 = checkisset($dinnerLabel);
 	$dayString .= "<input type=\"text\" name=" . $dinnerLabel . " value=\"$labelValue3\" class=\"recipeInput\" maxlength=\"50\">";
-			
+
 	$dayString .= "</fieldset>";
-	$dayString .= "</div>"; 
-	
+	$dayString .= "</div>";
+
 	return $dayString;
 };
 
@@ -104,63 +109,63 @@ function checkisset($label) {
 
 //FORM PROCESS
 if($_SERVER['REQUEST_METHOD'] == "POST"){
-	
-	
+
+
 	//Form process takes the info from the checkboxes and creates the calander days
 	if(isset($_POST['Create'])){
-		
+
 	};
 	$calanderString = "";
 	if(isset($_POST['sundayBox'])){
 		$currentDay = "Sunday";
 		$newDay = createCalanderDay($currentDay);
-		$calanderString .= $newDay; 
+		$calanderString .= $newDay;
 	}
 	if(isset($_POST['mondayBox'])){
 		$currentDay = "Monday";
 		$newDay = createCalanderDay($currentDay);
-		$calanderString .= $newDay; 
+		$calanderString .= $newDay;
 	}
 	if(isset($_POST['tuesdayBox'])){
 		$currentDay = "Tuesday";
 		$newDay = createCalanderDay($currentDay);
-		$calanderString .= $newDay; 
+		$calanderString .= $newDay;
 	}
 	if(isset($_POST['wednesdayBox'])){
 		$currentDay = "Wednesday";
 		$newDay = createCalanderDay($currentDay);
-		$calanderString .= $newDay; 
+		$calanderString .= $newDay;
 	}
 	if(isset($_POST['thursdayBox'])){
 		$currentDay = "Thursday";
 		$newDay = createCalanderDay($currentDay);
-		$calanderString .= $newDay; 
+		$calanderString .= $newDay;
 	}
 	if(isset($_POST['fridayBox'])){
 		$currentDay = "Friday";
 		$newDay = createCalanderDay($currentDay);
-		$calanderString .= $newDay; 
+		$calanderString .= $newDay;
 	}
 	if(isset($_POST['saturdayBox'])){
 		$currentDay = "Saturday";
 		$newDay = createCalanderDay($currentDay);
-		$calanderString .= $newDay; 
+		$calanderString .= $newDay;
 	}
-	
+
 	//FORM PROCESS if the form is posted through the search button, keep the calander and it's information
 	if(isset($_POST['search'])){
 		$type = $_POST['typeSelect'];
-		$catagory = $_POST['catagorySelect'];
+		//$catagory = $_POST['catagorySelect'];
 		$search = $_POST['searchBox'];
-		
-		function searchQuery($type, $catagory, $searchBox, $db) {
+
+		function searchQuery($type, /*$catagory,*/ $searchBox, $db) {
 			$type = $type;
-			$catagory = $catagory;
+			//$catagory = $catagory;
 			$search = $searchBox;
 			$returnString = '';
-		
+
 			if($type != 'tag') {
-				$sql = "SELECT DISTINCT recipe_info.recipe_name FROM recipe, recipe_info WHERE (recipe.recipe_name = recipe_info.recipe_name) AND (recipe_info.$type = '$search') AND (recipe.recipe_catagory = '$catagory') ";
+				$sql = "SELECT DISTINCT recipe_info.recipe_name FROM recipe, recipe_info WHERE (recipe.recipe_name = recipe_info.recipe_name) AND (recipe_info.$type = '$search')";
 				$result = $db->query($sql);
 				$returnString .= "<ul>";
 				while($rows = $result->fetch_row()){
@@ -174,7 +179,8 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 			}
 			return $returnString;
 		}
-	$searchResults = searchQuery($type, $catagory, $search, $db);
+
+	$searchResults = searchQuery($type, /*$catagory,*/ $search, $db);
 	}
 }
 
@@ -185,19 +191,19 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 
 	<meta charset="utf-8" >
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	
+
 	<!-- JQUERY -->
 	<link rel="stylesheet" href="http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-	
+
 	<!-- JQUERY UI -->
 	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-	
+
 	<!-- CSS -->
 	<link rel="stylesheet" href="css/mealprep.css">
-	
+
 	<title>mealprep</title>
-	
+
 </head>
 
 <body>
@@ -205,32 +211,32 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 <div id="wrapper">
 
 	<header>
-	
+
 		<h1>Meal Prep</h1>
-		
+
 		<nav>
 			<a href="newrecipe.html">New Recipe</a>
 			<a href="browse_recipes.php">Browse Recipes</a>
 			<a href="saved_plans.php">Saved Meal Plans</a>
 		</nav>
-		
+
 	</header>
-	
+
 	<content>
-	
-		
-	
-	
+
+
+
+
 		<!-- THE MASTER FORM!! -->
 		<form id="masterForm" action="" method="POST">
-		
+
 		<!-- form for choosing the days of the week -->
 		<div id="chooseDaysDiv">
 			<?php
 				echo $chooseDayString;
-			?>	
+			?>
 		</div>
-		
+
 		<!-- form for meal prep calander -->
 		<div id="calanderDiv">
 			<?php
@@ -238,29 +244,28 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 			?>
 		</div>
 			<fieldset>
-			
+
 			<!-- Search Box -->
 			<legend>Search Recipes</legend>
 				<label for="typeSelect">Select Search Type: </label>
 				<select name="typeSelect">
-					<option value="*"></option>
 					<option value="recipe_name">Recipe Name</option>
 					<option value="ingredient_name">Ingredient</option>
 					<option value="tag">Tag</option>
 				</select>
 				<label for="catagorySelect">Select Meal Type: </label>
-				<select name="catagorySelect">
+				<!--<select name="catagorySelect">
 					<option value=""></option>
 					<option value="breakfast">Breakfast</option>
 					<option value="lunch">Lunch</option>
 					<option value="dinner">Dinner</option>
-				</select>
+				</select>-->
 				<label for="searchBox"></label>
 				<input type="text" name="searchBox" maxlength="50">
 				<input type="submit" value="search" name="search">
 			</fieldset>
-			
-			
+
+
 		</form>
 
 		<!-- Display Recipes -->
@@ -269,15 +274,15 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 				echo $searchResults;
 			?>
 		</div>
-	
+
 	</content>
-	
+
 </div><!-- end wrapper -->
 
 <script>
-	
-	$("recipeInput").addClass("droppable");
-	
+
+	$(".recipeInput").addClass("droppable");
+
 	$( function() {
 		$( ".draggable" ).draggable({
 			cursor : 'pointer',
@@ -285,6 +290,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 			revertDuration : 250,
 			start: function( event, ui ) {
 				$dropText = $(this).text();
+				console.log($dropText);
 			}
 		});
 		$(".droppable").droppable({
@@ -292,9 +298,9 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 			tolerance : 'pointer',
 			drop : function(event, ui) {
 				$(this).val($dropText);
-				
+				console.log($dropText);
 			}
-		});	
+		});
 	} );
 </script>
 </body>
